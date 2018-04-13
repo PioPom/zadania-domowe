982edb0b83fce0335ec438676b58a9e233068407
PioPom <38221679+PioPom@users.noreply.github.com>
2018-04-13 09:47:06 +0200

    Add files via upload

---
+++ Zadanie domowe.py
@@ -0,0 +1,19 @@
+name = input("Proszę przepisz ten tekst uwględniając wielkość liter, oraz znaki interpunkcyjne \"Ala ma kota, a kot ma ale\" :")
+if name == (chr(65)+chr(108)+chr(97)+chr(32)+chr(109)+chr(97)+chr(32)+chr(107)+chr(111)+chr(116)+chr(97)+chr(44)+chr(32)+chr(97)+chr(32)+chr(107)+chr(111)+chr(116)+chr(32)+chr(109)+chr(97)+chr(32)+chr(97)+chr(108)+chr(101)) :
+    x = (chr(66)+chr(97)+chr(114)+chr(116)+chr(101)+chr(107)+chr(32)+chr(109)+chr(97)+chr(32)+chr(107)+chr(111)+chr(116)+chr(97)+chr(32)+chr(105)+chr(109)+chr(105)+chr(101)+chr(110)+chr(105)+chr(101)+chr(109)+chr(32)+chr(83)+chr(122)+chr(101)+chr(107)+chr(108)+chr(97))
+    print(x)
+else:
+    print("Upsss ! Cos poszlo nie tak ! :)")
+
+
+# Napis Bartek
+#(chr(65)+chr(108)+chr(97)+chr(32)+chr(109)+chr(97)+chr(32)+chr(107)+chr(111)+chr(116)+chr(97)+chr(44)+chr(32)+chr(97)+chr(32)+chr(107)+chr(111)+chr(116)+chr(32)+chr(109)+chr(97)+chr(32)+chr(97)+chr(108)+chr(101))
+
+#napis = input("Proszę przepisz ten tekst uwględniając wielkość liter, oraz znaki interpunkcyjne \"Ala ma kota, a kot ma ale\" :")
+
+#print(ord('A')) - uzyskanie liczby litery
+#x = "Ale ma kota, a kot ma ale"
+#sprawdzenie = (x == "Ala ma kota, a kot ma ale")
+#print (sprawdzenie)
+# napis Ala ma kota....
+#x = (chr(66)+chr(97)+chr(114)+chr(116)+chr(101)+chr(107)+chr(32)+chr(109)+chr(97)+chr(32)+chr(107)+chr(111)+chr(116)+chr(97)+chr(32)+chr(105)+chr(109)+chr(105)+chr(101)+chr(110)+chr(105)+chr(101)+chr(109)+chr(32)+chr(83)+chr(122)+chr(101)+chr(107)+chr(108)+chr(97))

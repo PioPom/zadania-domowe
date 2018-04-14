cc4efe6c75ad6a466856e4e4740fd0c1400871b7
PioPom <38221679+PioPom@users.noreply.github.com>
2018-04-14 13:50:38 +0200

    Add files via upload

---
+++ Zadanie domowe p.2.py
@@ -0,0 +1,6 @@
+napis = "Ala ma kota, a kot ma ale"
+print(napis)
+napis_bartek = (chr(66)+chr(97)+chr(114)+chr(116)+chr(101)+chr(107))
+napis_szekla = (chr(32)+chr(105)+chr(109)+chr(105)+chr(101)+chr(110)+chr(105)+chr(101)+chr(109)+chr(32)+chr(83)+chr(122)+chr(101)+chr(107)+chr(108)+chr(97))
+napis_ma_kota = (napis [4:11])
+print(napis_bartek +" "+ napis_ma_kota + napis_szekla)

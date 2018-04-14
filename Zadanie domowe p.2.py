a5ef77447b2677983d41d46b7866fad831897bcc
PioPom <38221679+PioPom@users.noreply.github.com>
2018-04-14 13:50:18 +0200

    Delete Zadanie domowe p.2.py

--- Zadanie domowe p.2.py
+++
@@ -1,6 +0,0 @@
-napis = "Ala ma kota, a kot ma ale"
-print(napis)
-x = (chr(66)+chr(97)+chr(114)+chr(116)+chr(101)+chr(107))
-y = (chr(32)+chr(105)+chr(109)+chr(105)+chr(101)+chr(110)+chr(105)+chr(101)+chr(109)+chr(32)+chr(83)+chr(122)+chr(101)+chr(107)+chr(108)+chr(97))
-z = (napis [4:11])
-print(x +" "+ z + y)
